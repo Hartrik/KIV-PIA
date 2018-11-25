330bde09fbc47350ba8e0901185604540996547f
Patrik Harag <patrikharag@gmail.com>
2018-11-25 12:12:15 +0100

    Refactor pagination

--- src/main/groovy/cz/hartrik/pia/controller/IBPagesController.groovy
+++ src/main/groovy/cz/hartrik/pia/controller/IBPagesController.groovy
@@ -4,7 +4,6 @@ import cz.hartrik.pia.ObjectNotFoundException
 import cz.hartrik.pia.dao.AccountDao
 import cz.hartrik.pia.dao.TransactionDao
 import cz.hartrik.pia.dto.Currency
-import cz.hartrik.pia.dto.User
 
 import cz.hartrik.pia.service.UserManager
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,8 +13,6 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-
-import javax.transaction.Transactional
 
 /**
  * Internet banking pages controller.
@@ -27,8 +24,7 @@ import javax.transaction.Transactional
 @RequestMapping("/ib")
 class IBPagesController {
 
-    private static def PAGINATION_VALUES = [25, 50, 100, 200]
-    private static def DEFAULT_PAGINATION = PAGINATION_VALUES[0]
+    private final def pagination = new PaginationHelper([25, 50, 100, 200], 25)
 
     @Autowired
     private UserManager userManager
@@ -47,7 +43,6 @@ class IBPagesController {
         return "edit-user"
     }
 
-    @Transactional
     @RequestMapping("accounts-overview")
     String accountsOverviewHandler(Model model) {
         def user = userManager.retrieveCurrentUser()
@@ -57,7 +52,6 @@ class IBPagesController {
         return "accounts-overview"
     }
 
-    @Transactional
     @RequestMapping("account/{id}")
     String accountHandler(Model model, @PathVariable Integer id,
             @RequestParam(required = false) Integer page,
@@ -66,7 +60,7 @@ class IBPagesController {
         def user = userManager.retrieveCurrentUser()
         def account = accountDao.findById(id)
                 .orElseThrow { new ObjectNotFoundException('Account not found') }
-        if (user.role != User.ROLE_ADMIN && account.owner.id != user.id) {
+        if (account.owner.id != user.id) {
             throw new AccessDeniedException("Cannot show other user's account")
         }
 
@@ -74,22 +68,9 @@ class IBPagesController {
         model.addAttribute('account', account)
 
         def transactions = transactionDao.findAllByAccount(account)
-        count = (!count || count < 1) ? DEFAULT_PAGINATION : count
-        page = (!page || page < 0) ? 0 : page
-        int pages = (transactions.size() + count - 1) / count
-        if (page >= pages) page = pages - 1
-        def transactionsView = transactions.subList(
-                Math.min(page * count, transactions.size()),
-                Math.min((page + 1) * count, transactions.size())
-        )
+        def transactionsView = pagination.paginate(model, "/ib/account/${id}", transactions, page, count)
         model.addAttribute('transactions', transactionsView)
 
-        def pageUrls = (0..<pages).collect { "/ib/account/${id}?page=$it&count=$count" }
-        model.addAttribute('pagination_pages', pageUrls)
-        model.addAttribute('pagination_current_page', pageUrls[page])
-        def countUrls = PAGINATION_VALUES.collect { [it, "/ib/account/${id}?page=0&count=$it"] }
-        model.addAttribute('pagination_count', countUrls)
-        model.addAttribute('pagination_current_count', countUrls[PAGINATION_VALUES.indexOf(count)])
         return "account"
     }
 
---
+++ src/main/groovy/cz/hartrik/pia/controller/PaginationHelper.groovy
@@ -0,0 +1,44 @@
+package cz.hartrik.pia.controller
+
+import org.springframework.ui.Model
+
+/**
+ *
+ * @version 2018-11-25
+ * @author Patrik Harag
+ */
+class PaginationHelper<T> {
+
+    final List<Integer> paginationValues
+    final int defaultPagination
+
+    PaginationHelper(List<Integer> paginationValues, int defaultPagination) {
+        this.paginationValues = new ArrayList<>(paginationValues)
+        this.defaultPagination = defaultPagination
+    }
+
+    List<T> paginate(Model model, String baseUrl, List<T> items, Integer page, Integer count) {
+        count = (!count || count < 1) ? defaultPagination : count
+        page = (!page || page < 0) ? 0 : page
+        int pages = (items.size() + count - 1) / count
+        if (page >= pages) page = pages - 1
+        def itemsView = items.subList(
+                Math.min(page * count, items.size()),
+                Math.min((page + 1) * count, items.size()))
+
+        fillPaginationAttributes(model, baseUrl, pages, page, count)
+
+        return itemsView
+    }
+
+    private void fillPaginationAttributes(Model model, String baseUrl, int pages, int currentPage, int count) {
+        def pageUrls = (0..<pages).collect { "${baseUrl}?page=$it&count=$count" }
+        model.addAttribute('pagination_pages', pageUrls)
+        model.addAttribute('pagination_current_page', pageUrls[currentPage])
+
+        def countUrls = paginationValues.collect { [it, "${baseUrl}?page=0&count=$it"] }
+        model.addAttribute('pagination_count', countUrls)
+        model.addAttribute('pagination_current_count', countUrls[paginationValues.indexOf(count)])
+    }
+
+}
