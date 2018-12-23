570cca7d735ab0f08d836b12686152b73f8a9d8b
Patrik Harag <patrikharag@gmail.com>
2018-12-23 21:44:45 +0100

    Fix beans collisions in integration test

--- src/main/groovy/cz/hartrik/pia/config/BaseConfig.java
+++ src/main/groovy/cz/hartrik/pia/config/BaseConfig.java
@@ -2,19 +2,27 @@ package cz.hartrik.pia.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Basic configuration.
  *
  * @author Patrik Harag
- * @version 2018-11-16
+ * @version 2018-12-23
  */
 @Configuration
 @ComponentScan(basePackages = {
-        "cz.hartrik.pia.config",
         "cz.hartrik.pia.controller",
         "cz.hartrik.pia.service",
         "cz.hartrik.pia.model",
+})
+@Import({
+        DatabaseConfig.class,
+        PersistenceConfig.class,
+        DatabasePopulator.class,
+        MvcConfig.class,
+        WebSecurityConfig.class,
+        TuringTestPopulator.class,
 })
 public class BaseConfig {
 
